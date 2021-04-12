91a028c545f9a36e173f7eb7d808e53ccfc2218a
Kyle <kylews@uw.edu>
2021-04-12 13:08:28 -0700

    Updated offsets

--- encoder_functions.py
+++ encoder_functions.py
@@ -27,7 +27,7 @@ def get_reading(arduino):
         reading = arduino.readline()
     return reading
 
-def get_encoder_feedback(arduino, num_encoders=2, joint_offsets=np.array([3.795, 1.5])):
+def get_encoder_feedback(arduino, num_encoders=2, joint_offsets=np.array([3.528, 1.5])):
     # joint_offsets - set in radians to change x/y position
     reading = get_reading(arduino)
     theta = decode_reading(reading, num_encoders=num_encoders)
