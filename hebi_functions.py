eee3bd3a308e5b8b72eed61ee85543fd63cec127
Kyle <kylews@uw.edu>
2021-04-20 15:23:26 -0700

    Updated offsets

--- hebi_functions.py
+++ hebi_functions.py
@@ -31,7 +31,7 @@ def initialize_hebi():
     command = hebi.GroupCommand(num_joints)
     return group, feedback, command
 
-def get_hebi_feedback(group, hebi_feedback, joint_offsets=np.array([-np.pi/2, -np.pi/2+0.3]), limit_stop=True, limits=np.array([np.pi+0.09, 2*np.pi-0.09])):
+def get_hebi_feedback(group, hebi_feedback, joint_offsets=np.array([-np.pi/2, 0.3]), limit_stop=True, limits=np.array([np.pi+0.09, 2*np.pi-0.09])):
     # joint_offsets - set in radians to change x/y position [rotation from hebi x, rotation from hebi y]
     # limit_stop currently limits position of both joints the same [lower limit, higher limit] in radians
     limit_stop_flag = False
@@ -48,7 +48,7 @@ def send_hebi_effort_command(group, command):
     group.send_command(command)
     return
 
-def send_hebi_position_command(group, command, joint_offsets=np.array([-np.pi/2, -np.pi/2+0.3])):
+def send_hebi_position_command(group, command, joint_offsets=np.array([-np.pi/2, 0.3])):
     # joint offset - set in radians to change x/y position
     # length of joint offsets must be consistent with number of joints
     offset_command = command.position
