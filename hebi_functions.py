d54f8ecc768f1a2e8c466f368b1910fc924b88c4
Kyle Schultz <kylews@uw.edu>
2021-06-08 12:50:57 -0700

    Older hebi functions

--- hebi_functions.py
+++ hebi_functions.py
@@ -1,10 +1,10 @@
 import hebi
 import numpy as np
-from time import sleep, time
+from time import sleep
 
 def get_group():
     families = ['arm']
-    names = ['Base', 'Elbow']
+    names = ["Base", 'Elbow']
     lookup = hebi.Lookup()
     sleep(2.0)
     group = lookup.get_group_from_names(families, names)
@@ -29,21 +29,20 @@ def initialize_hebi():
     feedback = hebi.GroupFeedback(group.size)
     num_joints = group.size
     command = hebi.GroupCommand(num_joints)
-    print('HEBIs initialized')
     return group, feedback, command
 
-def get_hebi_feedback(group, hebi_feedback, joint_offsets=np.array([-np.pi/2, 0.3])):
-    # joint_offsets - set in radians to change x/y position [rotation from hebi x, rotation from hebi y]    
-    backup_fbk = hebi_feedback
-    group.send_feedback_request()
-    hebi_feedback = group.get_next_feedback(reuse_fbk=hebi_feedback)  
-    if hebi_feedback is None:
-        hebi_feedback = backup_fbk
+def get_hebi_feedback(group, hebi_feedback, joint_offsets=np.array([-np.pi/2, 0.3]), limit_stop=True, limits=np.array([np.pi+0.09, 2*np.pi-0.09])):
+    # joint_offsets - set in radians to change x/y position [rotation from hebi x, rotation from hebi y]
+    # limit_stop currently limits position of both joints the same [lower limit, higher limit] in radians
+    limit_stop_flag = False
+    group.get_next_feedback(reuse_fbk=hebi_feedback)
     theta = np.array(hebi_feedback.position)
     omega = np.array(hebi_feedback.velocity)
     torque = np.array(hebi_feedback.effort)
     theta -= joint_offsets
-    return theta, omega, torque
+    if (theta > limits).any():
+        limit_stop_flag = True
+    return theta, omega, torque, limit_stop_flag
 
 def send_hebi_effort_command(group, command):
     group.send_command(command)
