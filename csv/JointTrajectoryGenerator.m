68fd62429c963542d21df8bc42091b9a6a8c6257
kyleschultz0 <49293521+kyleschultz0@users.noreply.github.com>
2021-07-19 15:14:04 -0700

    Added additional trajectories

--- csv/JointTrajectoryGenerator.m
+++ csv/JointTrajectoryGenerator.m
@@ -2,12 +2,12 @@
 
 clc; clear all; close all;
 
-f = 0.2;    % frequency (Hz)
+f = 0.5;    % frequency (Hz)
 A = 1.4;
-outputFile = "trajectories_sin02.csv";
+outputFile = "trajectories_sin05.csv";
 
-t = linspace(0, 20, 2001)';
-x1 = 0*t;
+t = linspace(0, 15, 2001)';
+x1 = 0.*t + pi/4;
 x2 = A*sin(2*pi*f*t);
 traj = [t x1 x2];
 
